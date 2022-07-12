d9230d334e34a0a1a3e98e3b93bda7061adad886
leilei <3053170581@qq.com>
2022-07-12 16:47:16 +0800

    second-commit

--- src/com/lw/nio/action/Action.java
+++ src/com/lw/nio/action/Action.java
@@ -6,6 +6,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
+//标注方法
 @Retention(RUNTIME)
 @Target(METHOD)
 public @interface Action {
